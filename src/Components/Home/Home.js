540697e356700d7dbb0b62c3e5fbf5fe92ce6067
Dhruv Patel <dhruv.patel.0220.dp@gmail.com>
2022-05-29 13:05:49 +0530

    added auto typing

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 import "./Home.css";
-import Profile from '../images/profile.jpeg'
+import Profile from "../images/profile.jpeg";
 
 const Home = () => {
   return (
     <section className="main">
       <div className="boxes">
         <h3>Dhruv Patel</h3>
-        <p>Web Developer</p>
+        <p>
+          I'm&nbsp;
+          <span style={{ color: "#4985c9", fontWieght: "bold" }}>
+            <Typewriter
+              loop
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+              words={["Frontend Developer", "Django Developer"]}
+            />
+          </span>
+        </p>
       </div>
       <img src={Profile} alt="Profile Photo " />
     </section>
